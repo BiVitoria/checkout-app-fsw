afd0135cd9fe53c69e5679815c2fb716c5003986
BiVitoria <biancavitoriavieira1@gmail.com>
2025-03-03 20:06:56 -0300

    fixing

---
+++ OneDrive/Área de Trabalho/fullstackweek-donalds/src/app/[slug]/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-options";
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findFirst({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      <div className="div space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar seua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-5 pt-14">
+        <ConsumptionMethodOption
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/hmbg.png"
+          option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+          buttonText="Para Levar"
+          imageAlt="Para levar"
+          imageUrl="/sacola.png"
+          option="TAKEAWAY"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
